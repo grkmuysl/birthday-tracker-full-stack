f3ecce1ee73327a79ecacf40930dd12068f19ef9
grkmuysl <gorkmuysal@gmail.com>
2026-03-14 17:24:39 +0300

    install brypt lib

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controller/authController");
+
+router.post("/register", authController.register);
+router.get("/get-users", authController.getAllUsers);
+
+module.exports = router;
