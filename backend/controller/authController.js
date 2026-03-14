297287a1f110595b4153ac557caedfb8224b396f
grkmuysl <gorkmuysal@gmail.com>
2026-03-14 17:38:42 +0300

    Adding login endpoint
    I added login endpoint and I used bcrypt and jwt to manage login process professionally

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,4 +1,6 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res, next) => {
   try {
@@ -24,6 +26,32 @@ exports.getAllUsers = async (req, res, next) => {
     res.status(200).json({
       message: "Getting all users successfully.",
       data: users,
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // check the user
+    const user = await User.findOne({ email });
+    if (!user) res.status(400).json({ message: "User not found" });
+
+    // check is matching
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) res.status(400).json({ message: "Wrong password!" });
+
+    // generate token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.json({
+      message: "loggin successfly",
+      token: token,
     });
   } catch (error) {
     res.status(400).json({ message: error.message });
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,5 +4,6 @@ const authController = require("../controller/authController");
 
 router.post("/register", authController.register);
 router.get("/get-users", authController.getAllUsers);
+router.post("/login", authController.login);
 
 module.exports = router;
